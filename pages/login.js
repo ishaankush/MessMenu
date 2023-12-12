2d2e1973ee38367597418a0cc80cd10fec162fa9
ishaankush <a.kushwaha5228@gmail.com>
2023-12-12 20:04:10 +0530

    initial setup route

---
+++ pages/login.js
@@ -0,0 +1,13 @@
+// pages/login.js
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Add your login form and logic here */}
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/signup.js
@@ -0,0 +1,13 @@
+// pages/signup.js
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div>
+      <h1>Signup Page</h1>
+      {/* Add your signup form and logic here */}
+    </div>
+  );
+};
+
+export default Signup;

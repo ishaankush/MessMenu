d52459f422805fad97cc66af2c8df953400cd91b
ishaankush <a.kushwaha5228@gmail.com>
2023-12-27 18:55:09 +0530

    firebase db read and create done

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -4,11 +4,17 @@ import {useAuthState} from 'react-firebase-hooks/auth'
 import {auth} from '../firebase/config'
 import { useRouter } from 'next/navigation';
 import { signOut } from 'firebase/auth';
+import { push,ref,set,get, DataSnapshot } from 'firebase/database';
+import { useState,useEffect } from 'react';
+import { database } from '../firebase/config';
 
 export default function Home() {
   const [user] = useAuthState(auth);
-  const router = useRouter()
+  const router = useRouter();
   const userSession = sessionStorage.getItem('user');
+  const [title,setTitle] =useState("");
+  const [desc,setDesc] = useState("");
+  const [users, setUsers] = useState([]);
 
   console.log({user})
  
@@ -16,6 +22,39 @@ export default function Home() {
     router.push('/sign-in')
   }
   
+  useEffect(() => {
+        const usersRef = ref(database, "users");
+        get(usersRef).then((snapshot) => {
+          if(snapshot.exists()){
+            const usersArray = Object.entries(snapshot.val()).map(([id, data]) => ({
+              id,
+              ...data,
+            }));
+            setUsers(usersArray);
+          } else{
+            console.log('no data received');
+          }
+          
+        }).catch((error) => {
+          console.error(error);
+        });
+      },[]);
+
+
+  const handleAddData =()=>{
+    try {
+      const usersRef = ref(database, "users");
+      const newDataRef = push(usersRef);
+
+      set(newDataRef,{
+        title: title,
+        desc: desc,
+      });
+      alert('Menu added successfully');
+    } catch (error) {
+      console.error('Firebase Error: ',error);
+    }
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -28,6 +67,26 @@ export default function Home() {
     className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded">
     Log out
     </button>
+
+    
+      <h1>Add Data </h1>
+      <div className='mb-4'>
+        <input type="text" placeholder='Title' value={title} onChange={(e)=> setTitle(e.target.value)} className='w-full border p-2 text-black' />
+        <input type="text" placeholder='Desc' value={desc} onChange={(e)=> setDesc(e.target.value)} className='w-full border p-2 text-black' />
+        <button onClick={handleAddData} className='bg-blue-500 text-white p-2 rounded' > Add menu Item</button>
+      </div>
+
+      <div>
+        <h2>Menu Items:</h2>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>
+              <strong>{user.title}</strong>: {user.desc}
+            </li>
+          ))}
+        </ul>
+      </div>
+
     <button
       onClick={() => {
         router.push('/');
--- app/firebase/config.js
+++ app/firebase/config.js
@@ -1,9 +1,11 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from 'firebase/auth'
+import {getDatabase} from 'firebase/database'
 
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL:"https://menu-app-f9e6a-default-rtdb.firebaseio.com/",
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
@@ -14,4 +16,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
 const auth = getAuth(app)
 
-export {app, auth}
+const database = getDatabase(app)
+
+export {app, auth, database}
